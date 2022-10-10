be81fa62778bf4bb15c1e5f166ce7fd9b26b0c01
Nikulin Danila <d220203@mail.ru>
2022-10-10 15:32:28 +0300

    Add lab3

--- lab3/lab_python_fp/process_data.py
+++ lab3/lab_python_fp/process_data.py
@@ -1,19 +1,39 @@
-import json
-from operator import concat
-from unique import Unique
-from field import field
+from print_result import print_result
+from cm_timer import cm_timer_1
 from gen_random import gen_random
+import json
+import sys
 
-def f1(a):
-    return Unique([i['job-name'] for i in field(data, 'job-name')], ignore_case=True)
-def f2(a):
-    return filter(lambda a: a.startswith('программист'), a)
-def f3(a):
-    return list(map(lambda x: concat(x, ' c опытом Python'), a))
-def f4(a):
-    return zip(a, gen_random(len(a),137287, 200000))
+try:
+    path = sys.argv[1]
+    print(path)
+except:
+    path = 'data_light.json'
 
-with open('data_light.json') as f:
-    data = json.loads(f.read())
-    for i in f4(f3(f2(f1(data)))):
-        print(i)
+with open(path) as f:
+    data = json.load(f)
+
+
+@print_result
+def f1(arg) -> list:
+    return sorted(list(set([el['job-name'] for el in arg])), key=lambda x: x.lower())
+
+
+@print_result
+def f2(arg) -> list:
+    return list(filter(lambda text: (text.split())[0].lower() == 'программист', arg))
+
+
+@print_result
+def f3(arg) -> list:
+    return list(map(lambda lst: lst + ' с опытом Python', arg))
+
+
+@print_result
+def f4(arg) -> list:
+    return list(zip(arg, ['зарплата ' + str(el) + ' руб.' for el in gen_random(len(arg), 100000, 200000)]))
+
+
+if __name__ == '__main__':
+    with cm_timer_1():
+        f4(f3(f2(f1(data))))
